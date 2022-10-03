736e0361f8cf3439d95ea20e76f6c4f0e45af105
Chi Ming Yip <cmyip99@gmail.com>
2022-10-03 23:31:55 +1100

    Facial recognition with canvas highlighting

--- script.js
+++ script.js
@@ -10,13 +10,20 @@ Promise.all([
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
-    (stream) => {
-      video.srcObject = stream;
-    },
+    stream => video.srcObject = stream;,
     err => console.error(err)
   );
 }
 
 video.addEventListener('playing', () => {
-  console.log('recognise my facceee');
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+  }, 100);
 });
